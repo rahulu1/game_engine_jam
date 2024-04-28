378f384c4a1c321520f4fe222ace5312b2b5d073
Rahul Unniyampath <rahulu@umich.edu>
2024-04-28 02:31:13 -0400

    preliminary CharacterController implementation

--- component_types/CharacterController.lua
+++ component_types/CharacterController.lua
@@ -1,10 +1,15 @@
 CharacterController = {
 
-	player_index = -1,
+	player_index = 0,
 	speed = 1000.0,
 	jump_power = 1000.0,
+	blocking_speed_multiplier = 0.5,
 
 	OnStart = function(self)
+		
+		Players = {}
+
+		Players[self.player_index] = self.actor
 
 		self.rb = self.actor:GetComponent("Rigidbody")
 		self.animator = self.actor:GetComponent("Animator")
@@ -13,23 +18,23 @@ CharacterController = {
 
 		if self.player_index == 1 then
 			
-			self.GetRight = function(self) return Input.GetKey( "right") end
-			self.GetLeft = function(self) return Input.GetKey( "left") end
-			self.GetUp = function(self) return Input.GetKey( "up") end
-			self.GetDown = function(self) return Input.GetKey( "down") end
+			self.GetRight = function() return Input.GetKeyDown("right") end
+			self.GetLeft = function() return Input.GetKeyDown("left") end
+			self.GetUp = function() return (Input.GetKeyDown("up") or Input.GetKeyDown("space")) end
+			self.GetDown = function() return Input.GetKeyDown("down") end
 
-			self.GetAttack = function(self) return Input.GetKey( "z") end
-			self.GetBlock = function(self) return Input.GetKey( "x") end
+			self.GetAttack = function() return Input.GetKeyDown("z") end
+			self.GetBlock = function() return Input.GetKeyDown("x") end
 
 		else
 
-			self.GetRight = function(self) return Input.GetButton("Right") end
-			self.GetLeft = function(self) return Input.GetButton("Left") end
-			self.GetUp = function(self) return Input.GetButton("Up") end
-			self.GetDown = function(self) return Input.GetButton("Down") end
+			self.GetRight = function() return Input.GetButtonDown("Right") end
+			self.GetLeft = function() return Input.GetButtonDown("Left") end
+			self.GetUp = function() return Input.GetButtonDown("Up") end
+			self.GetDown = function() return Input.GetButtonDown("Down") end
 
-			self.GetAttack = function(self) return Input.GetButton("RightShoulder") end
-			self.GetBlock = function(self) return Input.GetButton("LeftShoulder") end
+			self.GetAttack = function() return Input.GetButtonDown("RightShoulder") end
+			self.GetBlock = function() return Input.GetButtonDown("LeftShoulder") end
 		end
 
 
@@ -38,29 +43,35 @@ CharacterController = {
 	OnUpdate = function(self)
 
 		local curr_delta_time = Time.DeltaTime();
+
 		-- Horizontal
 		local horizontal_input = 0
-		if Input.GetKey("right") then
+
+		if self:GetRight() then
 			horizontal_input = self.speed * curr_delta_time
 		end
 
-		if Input.GetKey("left") then
+		if self:GetLeft() then
 			horizontal_input = -self.speed * curr_delta_time
+		end
+
+		if self:GetBlock() then
+			horizontal_input = horizontal_input * self.blocking_speed_multiplier
 		end
 
 		-- Vertical
 		local vertical_input = 0
 
 		-- Check on ground
-		local on_ground = false
-		ground_object = Physics.Raycast(self.rb:GetPosition(), Vector2(0, 1), 0.5)
-		if Input.GetKeyDown("up") or Input.GetKeyDown("space") then
+		local ground_object = Physics.Raycast(self.rb:GetPosition(), Vector2(0, 1), 0.5)
+		
+		if self.GetUp() or Input.GetKeyDown("space") then
 			if ground_object ~= nil then
 				vertical_input = -self.jump_power
+				-- TODO: Trigger jump animation
 			end
 		end
 
-		self.rb:AddForce(Vector2(horizontal_input, vertical_input))
+		self.rb:SetVelocity(Vector2(horizontal_input, vertical_input))
   end
 }
-

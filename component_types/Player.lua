78091a53e7075671088e4dbd112d13ef94b0d8c6
Rahul Unniyampath <rahulu@umich.edu>
2024-04-28 02:50:08 -0400

    add base player info to Player.lua

--- component_types/Player.lua
+++ component_types/Player.lua
@@ -2,11 +2,16 @@ Player = {
     player_index = 0,
 
     OnStart = function(self)
-		
-        if Players == nil then
-            Players = {}
-        end
 
 		Players[self.player_index] = self.actor
+
+        self.other_player_index = 1 - self.player_index
+    end,
+
+    OnUpdate = function(self)
+        
+        if self.other_player == nil then
+            self.other_player = Players[self.other_player_index]
+        end
     end
 }
